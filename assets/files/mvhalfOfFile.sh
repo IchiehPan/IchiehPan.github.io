9c82ee3a2821d3b630c7ce29e6ac6ca3118deb2b
ichieh <ichieh@foxmail.com>
2021-04-09 16:03:12 +0800

    linux 循环与移动文件

---
+++ assets/files/mvhalfOfFile.sh
@@ -0,0 +1,24 @@
+filename=`ls -t | head -20000`
+for name in $filename
+do
+  mv $name /Users/ichieh/Downloads/uploadImg0
+done
+
+filename=`ls -t | head -20000`
+for name in $filename
+do
+  mv $name /Users/ichieh/Downloads/uploadImg1
+done
+
+filename=`ls -t | head -20000`
+for name in $filename
+do
+  mv $name /Users/ichieh/Downloads/uploadImg2
+done
+
+filename=`ls -t | head -20000`
+for name in $filename
+do
+  mv $name /Users/ichieh/Downloads/uploadImg3
+done
+
